4de99547e966dcb3cf2713e19701f546e4378067
MMTrindade <matheus.tri.2010@gmail.com>
2023-02-28 17:30:32 +0000

    bug fix

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,9 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 //Need to import services of authentication and database from Firebase and
-import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -12,6 +13,13 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
+    /*apiKey: "AIzaSyCosjw6N1u4wGkFjW7w5yf_DNMxWg8fkj4",
+    authDomain: "letmeask-b210f.firebaseapp.com",
+    databaseURL: "https://letmeask-b210f-default-rtdb.firebaseio.com",
+    projectId: "letmeask-b210f",
+    storageBucket: "letmeask-b210f.appspot.com",
+    messagingSenderId: "467495226703",
+    appId: "1:467495226703:web:d9a69e51a282811128493a"*/
   };
   
   firebase.initializeApp(firebaseConfig);
